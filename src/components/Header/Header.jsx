e0ccf5f2fc775bc7f4f0c83bae8552d175f52438
Antônio Biasotti Amorim <102745879+antoniobiasotti@users.noreply.github.com>
2024-04-03 00:38:17 +0000

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import styles from "./header.module.css";
+import Image from "next/image";
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1>Where in the world?</h1>
+      <div className={styles.darkMode}>
+        <div>
+          <Image src="/lua.svg" width={15} height={15} alt="lua"></Image>
+          <p>Dark Mode</p>
+        </div>
+      </div>
+    </header>
+  );
+};
